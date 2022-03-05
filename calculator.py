bb9034c512ca0ecd895f7b3e8c57cf45a55b6833
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2022-03-05 14:31:57 +0100

    python switch version

--- calculator.py
+++ calculator.py
@@ -1,10 +1,8 @@
-
 import logging
-import numbers
 import sys
+
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-# Choose and check operation
 try:
     operation = int(input(
         " Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
@@ -13,52 +11,62 @@ except:
     print("Niepoprawny format")
     sys.exit(1)
 
-# Define calculating functions
 
-def calclulate_two_or_four(operation, first_num, second_num):
-    if operation == 2:
-        logging.info(f"Odejmuję {first_num} i {second_num}")
-        print(f"Wynik to {round(first_num - second_num, 2)}")
+def f(operation):
+    match operation:
+        case 1:
+            try:
+                numbs_list = [float(numbs_list)
+                              for numbs_list in input("Wprowadź wartości: ").split()]
+            except:
+                logging.info("Niepoprawny format")
+                sys.exit(1)
 
-    elif operation == 4:
-        logging.info(f"Dzielę {first_num} i {second_num}")
-        print(f"Wynik to {round(first_num / second_num, 2)}")
+            result = 0
+            for number in numbs_list:
+                result += number
+            print("Dodaję", end=' ')
+            for number in numbs_list:
+                print(number, end=' ')
+            logging.info(f"\nWynik to {result}")
+
+        case 2:
+            try:
+                first_num = float(input("Pierwsza liczba: "))
+                second_num = float(input("Druga liczba: "))
+            except:
+                logging.info("Niepoprawny format")
+                sys.exit(1)
+
+            logging.info(f"Odejmuję {first_num} i {second_num}")
+            logging.info(f"Wynik to {round(first_num - second_num, 2)}")
+
+        case 3:
+            try:
+                numbs_list = [float(numbs_list)
+                              for numbs_list in input("Wprowadź wartości: ").split()]
+            except:
+                logging.info("Niepoprawny format")
+                sys.exit(1)
+
+            result = 1
+            for number in numbs_list:
+                result *= number
+            print("Mnożę", end=' ')
+            for number in numbs_list:
+                print(number, end=' ')
+            logging.info(f"\nWynik to {result}")
+
+        case 4:
+            try:
+                first_num = float(input("Pierwsza liczba: "))
+                second_num = float(input("Druga liczba: "))
+            except:
+                print("Niepoprawny format")
+                sys.exit(1)
+
+            logging.info(f"Dzielę {first_num} i {second_num}")
+            logging.info(f"Wynik to {round(first_num / second_num, 2)}")
 
 
-def calclulate_one_or_three(operation, numbs_list):    
-    if operation == 1:
-        result = 0
-        for number in numbs_list:
-            result += number
-        print("Dodaję", end=' ')
-        for number in numbs_list:
-            print(number, end=' ')
-        print(f"\nWynik to {result}")
-
-    elif operation == 3:
-        result = 1
-        for number in numbs_list:
-            result *= number
-        print("Mnożę", end=' ')
-        for number in numbs_list:
-            print(number, end=' ')
-        print(f"\nWynik to {result}")
-
-# Choose and check and calculate 
-if operation == 2 or operation == 4:
-    try:
-        first_num = float(input("Pierwsza liczba: "))
-        second_num = float(input("Druga liczba: "))
-    except:
-        print("Niepoprawny format")
-        sys.exit(1)
-    calclulate_two_or_four(operation, first_num, second_num)
-
-elif operation == 1 or operation == 3:
-    try:
-        numbs_list = [float(numbs_list)
-                      for numbs_list in input("Wprowadź wartości: ").split()]
-    except:
-        print("Niepoprawny format")
-        sys.exit(1)
-    calclulate_one_or_three(operation, numbs_list)
+f(operation)
